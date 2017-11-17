5597542ed9f4600d4cd039a4a9c107bb0b0c1d7f
fisher.yu <useryu@qq.com>
2017-11-17 20:21:13 +0800

    remove unused import

--- src/main/java/com/fisher/blockchain/controller/BlockController.java
+++ src/main/java/com/fisher/blockchain/controller/BlockController.java
@@ -3,7 +3,6 @@ package com.fisher.blockchain.controller;
 import java.util.LinkedList;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;  
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
